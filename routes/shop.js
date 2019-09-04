7bac863f7966982a10cecdcaa3e7b943dea8b9e5
Freeman99sd <freeman99sd@163.com>
2019-09-04 13:58:19 +0800

    aa

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;

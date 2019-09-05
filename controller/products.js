5e20b74cc0ac980898b22a8a0f3980f8913ba89e
freeman99sd <freeman99sd@gmail.com>
2019-09-05 23:28:00 +0800

    a

--- controller/products.js
+++ controller/products.js
@@ -1,24 +1,24 @@
-import Product, { fetchAll } from '../models/product';
+const Product = require('../models/product');
 
-export function getAddProduct(req, res, next) {
+exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   });
-}
+};
 
-export function postAddProduct(req, res, next) {
+exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
-}
+};
 
-export function getProducts(req, res, next) {
-  fetchAll(products => {
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
   });
-}
+};

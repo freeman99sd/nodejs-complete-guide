40bf2b1ee34f7abb026df2affc9a0944c0c7b123
freeman99sd <freeman99sd@gmail.com>
2019-09-07 11:14:50 +0800

    aa

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
-const productsController = require('../controller/products');
+const adminController = require('../controller/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;

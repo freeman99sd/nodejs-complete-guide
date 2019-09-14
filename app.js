27e644a4647c58244a92aa6af0555bd3879200f6
freeman99sd <freeman99sd@gmail.com>
2019-09-14 21:50:41 +0800

    asd

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -34,6 +36,9 @@ app.use(
   })
 );
 
+app.use(csrf());
+app.use(flash());
+
 app.use((req, res, next) => {
   if (!req.session.user) {
     next();
@@ -49,6 +54,11 @@ app.use((req, res, next) => {
   }
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,37 +3,67 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    errorMessage: message
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        req.flash('error', 'Invalid email or password!');
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          req.flash('error', 'Invalid email or password!');
+          return res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
   const password = req.body.password;
-  const confirmPassword = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
   const email = req.body.email;
   User.findOne({ email })
     .then(userDoc => {
@@ -41,13 +71,20 @@ exports.postSignup = (req, res, next) => {
         return bcrypt
           .hash(password, 12)
           .then(hashedPassword => {
-            const user = new User({ email, password, cart: { items: [] } });
+            console.log(hashedPassword);
+            const user = new User({
+              email,
+              password: hashedPassword,
+              cart: { items: [] }
+            });
+            console.log(user);
             return user.save();
           })
           .then(result => {
             res.redirect('/login');
           });
       }
+      req.flash('error', 'email already exists');
       return res.redirect('/signup');
     })
 
